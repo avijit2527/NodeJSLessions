30143a7c357167bd7104b88f02259d9bde761377
avijit2527 <iamavijitroy@gmail.com>
2020-05-03 01:44:21 +0530

    file upload

--- conFusionServer/app.js
+++ conFusionServer/app.js
@@ -15,6 +15,8 @@ var usersRouter = require('./routes/users');
 const dishRouter = require('./routes/dishRouter');
 const promotionRouter = require('./routes/promoRouter');
 const leaderRouter = require('./routes/leaderRouter');
+const uploadRouter = require('./routes/uploadRouter');
+
 
 const mongoose = require('mongoose');
 
@@ -59,6 +61,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/dishes', dishRouter);
 app.use('/promotions', promotionRouter);
 app.use('/leaders', leaderRouter);
+app.use('/imageUpload', uploadRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
